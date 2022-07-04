1d24c41d4a3268550ce7a38a9e28477388e3fbe4
Sanjibon <Sanjibon@Deb.home>
2022-07-03 23:32:23 -0400

    This project is created by Deb

---
+++ Badstudent/src/test/java/learner/BadStudent.java
@@ -0,0 +1,14 @@
+package learner;
+
+public class BadStudent {
+	int number=7;
+	static String name="deb";
+
+	public static void main(String[] args) {
+	double result=10.99;
+	
+}
+public void result() {
+	System.out.println(number);
+}
+}
